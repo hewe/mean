48c063a8d20ea8f37d42b07c1a6a2f74c5028f39
Weiping He <weipingx.he@gmail.com>
2015-02-12 07:36:07 +0000

    made grunt to startup mongodb and nodemon concurrently through tasks

--- gruntfile.js
+++ gruntfile.js
@@ -1,16 +1,44 @@
 module.exports = function(grunt){
     grunt.initConfig({
-       nodemon: {
+        concurrent: {
+            tasks: ['shell:mongodb', 'nodemon'],
+            options: {
+                logConcurrentOutput: true
+            }
+        },
+        shell: {
+            mongodb: {
+                command: 'mongod --dbpath /home/ubuntu/workspace/db --nojournal',
+                options: {
+                    async: true,
+                    stdout: false,
+                    stderr: true,
+                    failOnError: true,
+                    execOptions: {
+                        cwd: '.'
+                    }
+                }
+            }
+        },
+        nodemon: {
             all: {
                script: 'server.js',
                options: {
                    watchedExtensions : ['js']
                }
             }
-       }, 
-        
+       }
     });
     
+    
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-shell');
     grunt.loadNpmTasks('grunt-nodemon');
-    grunt.registerTask('default', ['nodemon']);
+    
+    grunt.registerTask('default', ['concurrent']);
+    
+    /*
+     *this will do samething as above because without specifying an individual task inside concurrent, grunt will execute all tasks
+     * grunt.registerTask('default', ['concurrent:tasks'])
+     */
 };
